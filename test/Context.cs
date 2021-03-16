6415474922804b9f941cdba917514a1e31474bd3
ShadyShawkat <shadyshawkat001@gmail.com>
2021-03-16 17:41:17 +0200

    Add project files.

---
+++ test/Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    public class Context : DbContext
+    {
+        public Context():base("Data Source=.;Initial Catalog=dbdbdbDB;Integrated Security=True")
+        {
+
+        }
+
+        public virtual DbSet<Employee> employees { get; set; }
+    }
+}
---
+++ test/Migrations/202103161539478_init.cs
@@ -0,0 +1,26 @@
+﻿namespace test.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        name = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Employees");
+        }
+    }
+}
